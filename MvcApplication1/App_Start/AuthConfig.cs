acd6ccd0ded10f7c4ac0235cdd9c24fe3c30e2bd
hannahhannah <cerberusjump27@rocketmail.com>
2015-03-20 20:35:04 -0700

    Initial commit

---
+++ MvcApplication1/App_Start/AuthConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Web.WebPages.OAuth;
+using MvcApplication1.Models;
+
+namespace MvcApplication1
+{
+    public static class AuthConfig
+    {
+        public static void RegisterAuth()
+        {
+            // To let users of this site log in using their accounts from other sites such as Microsoft, Facebook, and Twitter,
+            // you must update this site. For more information visit http://go.microsoft.com/fwlink/?LinkID=252166
+
+            OAuthWebSecurity.RegisterMicrosoftClient( 
+                  clientId: "0000000040146604",   
+                  clientSecret: "YfvyZDPEuhbMA4b0HCxLQN-Z-hr2MRVk");
+
+          OAuthWebSecurity.RegisterTwitterClient(
+              consumerKey: "rci7Slsz4pFZTpYT0fcfbTyCE",
+            consumerSecret: "EeMhh29OxVtEFCFln7PjX0GfFv4gxJAgCmUnDBYtMhPHGTSFMK");
+
+          //  consumerKey: "KJJJ8K6ywqpHIStFpZZbX3erB",
+          //  consumerSecret: "iCmyYuH2L5nBN0PnHoocPGG30Y2UgiZKu0EdaMWroDm91fNqVU");
+
+           OAuthWebSecurity.RegisterFacebookClient(
+                   appId: "664835416976040",
+                    appSecret: "362932536d56683b11af9470c7c29579");
+
+         //  OAuthWebSecurity.RegisterGoogleClient( );
+        }
+    }
+}
---
+++ MvcApplication1/Controllers/MembershipServiceController.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Web.Mvc;
+using System.Web.Security;
+using System.Web.UI;
+using MembershipService.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    /// <summary>
+    /// HTTP API to membership/roles/profiles
+    /// Returns relevant HTTP status codes to indicate success (200) or failure (401/404)
+    /// </summary>
+    [DoNotRedirectToLogin, OutputCache(Location = OutputCacheLocation.None)] 
+    public class MembershipServiceController : Controller
+    {
+        // GET /MembershipService/LoggedOnAs
+        public string LoggedOnAs()
+        {
+            return User.Identity.Name;
+        }
+
+        // POST /MembershipService/Register?name=someuser&password=somepassword&email=email@example.com&passwordQuestion=some+question&passwordAnswer=some+Answer
+        // Note that the parameters may be given either in the querystring or in the POST body (a.k.a. Request.Form)
+        [HttpPost] public HttpStatusCodeResult Register(string name, string password, string email, string passwordQuestion, string passwordAnswer)
+        {
+            MembershipCreateStatus createStatus;
+            Membership.CreateUser(name, password, email, passwordQuestion, passwordAnswer, isApproved: true, providerUserKey: null, status: out createStatus);
+
+            if (createStatus == MembershipCreateStatus.Success)
+                return new HttpStatusCodeResult(200, "OK");
+            else
+                return new HttpStatusCodeWithBodyResult(400, null, createStatus.ToString());
+        }
+
+        // POST /MembershipService/LogOn?name=myname&password=mypassword
+        // Note that the parameters may be given either in the querystring or in the POST body (a.k.a. Request.Form)
+        [HttpPost] public HttpStatusCodeResult LogOn(string name, string password)
+        {
+            if (Membership.ValidateUser(name, password)) {
+                FormsAuthentication.SetAuthCookie(name, createPersistentCookie: false);
+                return new HttpStatusCodeResult(200, "OK");
+            }
+
+            return new HttpStatusCodeResult(401);
+        }
+
+        // POST /MembershipService/LogOut
+        // Note that the parameters may be given either in the querystring or in the POST body (a.k.a. Request.Form)
+        [HttpPost] public HttpStatusCodeResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            return new HttpStatusCodeResult(200);
+        }
+
+        // GET /MembershipService/Roles
+        public string Roles()
+        {
+            // Returns a list of roles for the current visitor, one role per line
+            return string.Join(Environment.NewLine, System.Web.Security.Roles.GetRolesForUser());
+        }
+
+        // GET /MembershipService/Profile/someKey
+        [HttpGet] public ActionResult Profile(string key)
+        {
+            object propertyValue = HttpContext.Profile[key];
+
+            if (propertyValue == null)
+                return HttpNotFound();
+
+            return Content(propertyValue.ToString());
+        }
+
+        // POST /MembershipService/Profile/someKey?value=myValue
+        // Note that the parameter may be given either in the querystring or in the POST body (a.k.a. Request.Form)
+        [HttpPost] public HttpStatusCodeResult Profile(string key, string value)
+        {
+            HttpContext.Profile[key] = value;
+            return new HttpStatusCodeResult(200);
+        }
+    }
+}
